1607e47379ac59b185f59420c71bdc568c0e3b86
gasweld <gasweld@gmail.com>
2019-10-08 01:35:44 +0300

    Moved sockets code to a file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const ws = require("ws");
 const fs = require("fs");
 const util = require("util");
-const { Grooming } = require("./server/grooming");
+const socketsInterface = require("./server/sockets-interface");
 
 const p = util.promisify;
 const readFile = p(fs.readFile);
@@ -27,75 +27,6 @@ const server = http.createServer(function(req, res) {
   res.end();
 });
 const wss = new ws.Server({ server });
-
-const grooming = new Grooming();
-const sockets = [];
-
-function sendAll(type, val) {
-  const s = JSON.stringify({ type, val });
-  sockets.forEach(ws => ws.send(s));
-}
-
-function processSocket(ws) {
-  let user;
-  function send(type, val) {
-    ws.send(JSON.stringify({ type, val }));
-  }
-
-  const handlers = {
-    echo(val) {
-      send("ohce", val);
-    },
-    auth(val) {
-      user = val;
-      send("init", grooming.state);
-    },
-    chat(val) {
-      const msg = {
-        author: user,
-        text: val
-      };
-      grooming.chat(msg);
-      sendAll("chat", msg);
-    },
-    toggleAdvice(val) {
-      const { ticketId, advice } = val;
-      if (grooming.toggleAdvice(ticketId, advice)) {
-        sendAll("tickets", grooming.state.tickets);
-      }
-    },
-    toggleVote(val) {
-      const { ticketId, score } = val;
-      if (grooming.toggleVote(user, ticketId, score)) {
-        sendAll("tickets", grooming.state.tickets);
-      }
-    },
-    deleteTicket(val) {
-      const { ticketId } = val;
-      if (grooming.deleteTicket(ticketId)) {
-        sendAll("tickets", grooming.state.tickets);
-      }
-    },
-    createTicket(val) {
-      const { title } = val;
-      grooming.createTicket(title);
-      sendAll("tickets", grooming.state.tickets);
-    }
-  };
-  ws.on("message", function incoming(message) {
-    const msg = JSON.parse(message);
-    console.log(msg);
-    handlers[msg.type](msg.val);
-  });
-}
-
-wss.on("connection", function connection(ws) {
-  sockets.push(ws);
-  ws.on("close", function() {
-    const pos = sockets.indexOf(ws);
-    sockets.splice(pos, 1);
-  });
-  processSocket(ws);
-});
+socketsInterface(wss);
 
 server.listen(process.env.PORT || 8080);
---
+++ server/sockets-interface.js
@@ -0,0 +1,86 @@
+const { Grooming } = require("./grooming");
+
+module.exports = function socketsInterface(wss) {
+  /**
+   * A Grooming instance for the current session.
+   */
+  const grooming = new Grooming();
+
+  /**
+   * The list of all connected sockets.
+   */
+  const sockets = [];
+
+  wss.on("connection", function connection(ws) {
+    sockets.push(ws);
+    ws.on("close", function() {
+      const pos = sockets.indexOf(ws);
+      sockets.splice(pos, 1);
+    });
+    processSocket(ws);
+  });
+
+  function processSocket(ws) {
+    let user;
+    function send(type, val) {
+      ws.send(JSON.stringify({ type, val }));
+    }
+
+    const handlers = {
+      echo(val) {
+        send("ohce", val);
+      },
+      auth(val) {
+        user = val;
+        send("init", grooming.state);
+      },
+      chat(val) {
+        const msg = {
+          author: user,
+          text: val
+        };
+        grooming.chat(msg);
+        sendAll("chat", msg);
+      },
+      toggleAdvice(val) {
+        const { ticketId, advice } = val;
+        if (grooming.toggleAdvice(ticketId, advice)) {
+          sendAll("tickets", grooming.state.tickets);
+        }
+      },
+      toggleVote(val) {
+        const { ticketId, score } = val;
+        if (grooming.toggleVote(user, ticketId, score)) {
+          sendAll("tickets", grooming.state.tickets);
+        }
+      },
+      deleteTicket(val) {
+        const { ticketId } = val;
+        if (grooming.deleteTicket(ticketId)) {
+          sendAll("tickets", grooming.state.tickets);
+        }
+      },
+      createTicket(val) {
+        const { title } = val;
+        grooming.createTicket(title);
+        sendAll("tickets", grooming.state.tickets);
+      }
+    };
+    ws.on("message", function incoming(message) {
+      const msg = JSON.parse(message);
+      console.log(msg);
+      handlers[msg.type](msg.val);
+    });
+  }
+
+  /**
+   * Sends a message to all connected sockets.
+   *
+   * @param {string} type Message type
+   * @param {any} val Message payload
+   */
+  function sendAll(type, val) {
+    const s = JSON.stringify({ type, val });
+    sockets.forEach(ws => ws.send(s));
+  }
+};
