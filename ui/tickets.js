7c1d4a34b927b113658a2a00d8f3902e2e0c2998
gasweld <gasweld@gmail.com>
2019-10-10 00:35:35 +0300

    Code cleanup

--- ui/tickets.js
+++ ui/tickets.js
@@ -1,5 +1,8 @@
+// const ding = new Audio(
+//   "https://notificationsounds.com/soundfiles/1bb91f73e9d31ea2830a5e73ce3ed328/file-sounds-1091-graceful.mp3"
+// );
+
 export default function initTickets(grooming, container) {
-  const scores = [1, 2, 3, 5, 8, 13];
   grooming.onLoad(function(state) {
     container.innerHTML =
       state.tickets.map(renderTicket).join("") +
@@ -48,70 +51,73 @@ export default function initTickets(grooming, container) {
     const { ticketId } = event.target.dataset;
     grooming.deleteTicket(ticketId);
   });
+}
 
-  function renderTicket(ticket) {
-    const votes = score =>
-      ticket.votes
-        .filter(vote => vote.score == score)
-        .map(vote => vote.author)
-        .join(", ");
-    return `<div class="ticket">
-                  <h3>${ticket.title} <button name="delete" data-ticket-id="${
-      ticket.id
-    }">&times;</button></h3>
-                  <div>
-                  ${scores
-                    .map(
-                      score =>
-                        `<div><button class="score" data-ticket-id="${
-                          ticket.id
-                        }" data-score="${score}" type="button">${score}</button> ${votes(
-                          score
-                        )}</div>`
-                    )
-                    .join("")}
-                  </div>
-                  <div>
-                      ${renderAdvices(ticket)}
-                  </div>
-              </div>`;
-  }
+const scores = [1, 2, 3, 5, 8, 13];
 
-  function renderAdvices(ticket) {
-    const advices = [
-      "it's very easy",
-      "пропозаль",
-      "папап",
-      "I'm not sure",
-      "it's a different card",
-      "there's a card for that",
-      "copypest",
-      "exactly",
-      "I have to look at the code",
-      "makes sense",
-      "does not make sense"
-    ];
-    const is = advice => ticket.advices.includes(advice);
-    const id = i => `tn-${ticket.id}-${i}`;
+function renderTicket(ticket) {
+  const votes = score =>
+    ticket.votes
+      .filter(vote => vote.score == score)
+      .map(vote => vote.author)
+      .join(", ");
+  return `<div class="ticket">
+                <h3>${ticket.title} <button name="delete" data-ticket-id="${
+    ticket.id
+  }">&times;</button></h3>
+                <div>
+                ${scores
+                  .map(
+                    score =>
+                      `<div><button class="score" data-ticket-id="${
+                        ticket.id
+                      }" data-score="${score}" type="button">${score}</button> ${votes(
+                        score
+                      )}</div>`
+                  )
+                  .join("")}
+                </div>
+                <div>
+                    ${renderTechnicalDetails(ticket)}
+                </div>
+            </div>`;
+}
 
-    return (
-      `<table>
-        <caption>Implementation notes</caption>
-        ` +
-      advices
-        .map(
-          (advice, i) =>
-            `<tr><td><label for="${id(i)}">${advice}</label></td>
-              <td>
-              <input id="${id(i)}" data-ticket-id="${
-              ticket.id
-            }" data-advice="${advice}" class="advice-toggle" type="checkbox" ${
-              is(advice) ? "checked" : ""
-            }>
-              </td></tr>`
-        )
-        .join("") +
-      "</table>"
-    );
-  }
+const specifications = [
+  "it's very easy",
+  "пропозаль",
+  "папап",
+  "I'm not sure",
+  "it's a different card",
+  "there's a card for that",
+  "copypest",
+  "exactly",
+  "I have to look at the code",
+  "makes sense",
+  "does not make sense"
+];
+
+function renderTechnicalDetails(ticket) {
+  const is = advice => ticket.advices.includes(advice);
+  const id = i => `tn-${ticket.id}-${i}`;
+
+  return (
+    `<table>
+      <caption>Implementation notes</caption>
+      ` +
+    specifications
+      .map(
+        (advice, i) =>
+          `<tr><td><label for="${id(i)}">${advice}</label></td>
+            <td>
+            <input id="${id(i)}" data-ticket-id="${
+            ticket.id
+          }" data-advice="${advice}" class="advice-toggle" type="checkbox" ${
+            is(advice) ? "checked" : ""
+          }>
+            </td></tr>`
+      )
+      .join("") +
+    "</table>"
+  );
 }
