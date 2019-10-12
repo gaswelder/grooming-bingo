afc54b6966ef931d62fe09772438b32806db9073
gasweld <gasweld@gmail.com>
2019-10-12 16:02:34 +0300

    Top-level ui component

--- ui/index.js
+++ ui/index.js
@@ -1,21 +1,11 @@
-import Chat from "./chat/chat.svelte";
-import Banner from "./banner.svelte";
 import { Grooming } from "./grooming";
-import Tickets from "./tickets/tickets.svelte";
+import UI from "./ui.svelte";
 
 // const ding = new Audio(
 //   "https://notificationsounds.com/soundfiles/1bb91f73e9d31ea2830a5e73ce3ed328/file-sounds-1091-graceful.mp3"
 // );
 
 window.initGrooming = function(root) {
-  const chatRoot = document.createElement("div");
-  chatRoot.className = "chatRoot";
-  const ticketsRoot = document.createElement("div");
-  ticketsRoot.className = "ticketsRoot";
-
-  root.appendChild(ticketsRoot);
-  root.appendChild(chatRoot);
-
   let username = localStorage.getItem("username");
   while (!username) {
     username = this.prompt("please enter your name");
@@ -24,36 +14,19 @@ window.initGrooming = function(root) {
     }
   }
 
-  Notification.requestPermission();
-
   const grooming = new Grooming(username);
 
-  new Chat({
-    target: chatRoot,
-    props: {
-      grooming
-    }
-  });
-
+  Notification.requestPermission();
   grooming.onChatMessage(message => {
     if (document.hidden && Notification.permission == "granted") {
       new Notification(`${message.author}: ${message.text}`);
     }
   });
 
-  new Tickets({
-    target: ticketsRoot,
-    props: { grooming }
-  });
-
-  const banner = new Banner({
-    target: document.body,
+  new UI({
+    target: root,
     props: {
-      online: false
+      grooming
     }
-  });
-
-  grooming.onConnectionChange(function(online) {
-    banner.$set({ online });
   });
 };
---
+++ ui/ui.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Chat from "./chat/chat.svelte";
+  import Tickets from "./tickets/tickets.svelte";
+  import Banner from "./banner.svelte";
+
+  export let grooming;
+
+  let online = false;
+
+  grooming.onConnectionChange(function(newOnline) {
+    online = newOnline;
+  });
+</script>
+
+<div class="ticketsRoot">
+  <Tickets {grooming} />
+</div>
+<div class="chatRoot">
+  <Chat {grooming} />
+</div>
+<Banner {online} />
