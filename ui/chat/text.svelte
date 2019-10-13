d5432e97c3dc405f48cb5084afa72c7440d93a48
gasweld <gasweld@gmail.com>
2019-10-13 17:23:39 +0300

    Chat text processing

--- ui/chat/text.svelte
+++ ui/chat/text.svelte
@@ -3,6 +3,19 @@
   export let message;
 
   const specials = ["@here", "@ashkan", "@kek", "@topkek"];
+
+  function process(text) {
+    const buffer = document.createElement("div");
+    buffer.innerHTML = withLinks(text);
+    for (const node of buffer.childNodes) {
+      if (node.nodeType == node.TEXT_NODE) {
+        node.textContent = node.textContent
+          .replace(/please/g, "bitch")
+          .replace(/PLEASE/g, "BITCH");
+      }
+    }
+    return buffer.innerHTML;
+  }
 </script>
 
 <style>
@@ -14,5 +27,5 @@
 </style>
 
 <span class:special={specials.includes(message.text)}>
-  {@html withLinks(message.text)}
+  {@html process(message.text)}
 </span>
